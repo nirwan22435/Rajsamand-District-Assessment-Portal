c2bcfe234a5d49538f4b17445f4c686b8f1fa0c0
Devkaran Nirwan <64684340+nirwan22435@users.noreply.github.com>
2026-08-03 11:09:37 +0530

    build: update API routing and migrate to ES modules
    Configure Netlify redirects for API functions and update the email notification function to use ES module syntax.

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-exports.handler = async function (event, context) {
+export const handler = async function (event, context) {
   // CORS Headers
   const headers = {
     'Access-Control-Allow-Origin': '*',
